852af36849ffa6f539f555609d88a1aad8b01609
Claudio Borromei <cborrome@c1r5p17.hive.fi>
2025-05-13 11:32:28 +0300

    agrs fix

--- srcs/05_builtin/builtin.c
+++ srcs/05_builtin/builtin.c
@@ -15,22 +15,21 @@ int	is_builtin(char *cmd)
 	);
 }
 
-int	execute_builtin(char **argv, char ***envp_new, int exit_status, \
-	int *should_exit, t_token *token)
+int	execute_builtin(t_cmd *cmd, char ***envp_new, int exit_status, t_token *token)
 {
-	if (ft_strcmp(argv[0], "echo") == 0)
-		return (builtin_echo(argv, token));
-	if (ft_strcmp(argv[0], "cd") == 0)
-		return (builtin_cd(argv));
-	if (ft_strcmp(argv[0], "pwd") == 0)
-		return (builtin_pwd(argv));
-	if (ft_strcmp(argv[0], "export") == 0)
-		return (builtin_export(argv, envp_new));
-	if (ft_strcmp(argv[0], "unset") == 0)
-		return (builtin_unset(argv, envp_new));
-	if (ft_strcmp(argv[0], "env") == 0)
-		return (builtin_env(argv, envp_new));
-	if (ft_strcmp(argv[0], "exit") == 0)
-		return (builtin_exit(argv, exit_status, should_exit));
+	if (ft_strcmp(cmd->argv[0], "echo") == 0)
+		return (builtin_echo(cmd->argv, token));
+	if (ft_strcmp(cmd->argv[0], "cd") == 0)
+		return (builtin_cd(cmd->argv));
+	if (ft_strcmp(cmd->argv[0], "pwd") == 0)
+		return (builtin_pwd(cmd->argv));
+	if (ft_strcmp(cmd->argv[0], "export") == 0)
+		return (builtin_export(cmd->argv, envp_new));
+	if (ft_strcmp(cmd->argv[0], "unset") == 0)
+		return (builtin_unset(cmd->argv, envp_new));
+	if (ft_strcmp(cmd->argv[0], "env") == 0)
+		return (builtin_env(cmd->argv, envp_new));
+	if (ft_strcmp(cmd->argv[0], "exit") == 0)
+		return (builtin_exit(cmd->argv, exit_status, cmd));
 	return (1);
 }
